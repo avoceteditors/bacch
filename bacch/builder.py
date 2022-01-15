739575af1cf7135e8bc661415e782502276359af
Kenneth P. J. Dyer <kenneth@avoceteditors.com>
2022-01-15 16:56:26 -0500

    Updates

--- bacch/builder.py
+++ bacch/builder.py
@@ -6,6 +6,7 @@ logger = logging.getLogger("bacch")
 class BacchBuilder(Builder):
     name = "bacch"
     format = "latex"
+    allow_parallel = True
     epilog = "Bacch LaTeX Build is available in %(outdirs)s"
 
     def init(self):
@@ -15,10 +16,12 @@ class BacchBuilder(Builder):
         self.build_map = False
         texescape.init()
 
-
     def build_all(self):
         self.build_map = True
         self.build()
+
+    def build_specific(files):
+        pass
 
 
     def init_document_data(self):
